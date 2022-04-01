3d9eb55e6c75e1c7b77f8d203378bf38a8bfb3a8
Xueyd <47029083+Xue-yd@users.noreply.github.com>
2022-04-01 14:37:56 +0800

    Add files via upload

---
+++ longestORF.py
@@ -0,0 +1,28 @@
+#reference：https://www.biostars.org/p/104936/
+#usage: python longestORF.py allORF.fasta > out_longestORF.fasta
+
+import sys
+from Bio import SeqIO
+
+currentCid = ''
+buffer = []
+
+for record in SeqIO.parse(open("C:\\Users\\17710\\Desktop\\7.orf.fasta"),"fasta"):
+    cid = str(record.description).split(':')[0][1:]
+
+    if currentCid == '':
+        currentCid = cid
+        buffer.append((str(record.description), str(record.seq)))
+    else:
+        if cid != currentCid:
+            buffer.sort(key = lambda x : len(x[1]))
+            print ('>' + buffer[-1][0])
+            print (buffer[-1][1])
+            currentCid = cid
+            buffer = [(str(record.description),str(record.seq))]
+        else:
+            buffer.append((str(record.description),str(record.seq)))
+
+buffer.sort(key = lambda x : len(x[1]))
+print ('>' + buffer[-1][0])
+print (buffer[-1][1])
